812c0e5e0d815ef0f90086d03bebce77732a689b
Adam P <adam.porbanderwalla@gmail.com>
2026-03-22 20:37:40 -0400

    feat: started working on main & added chi for routing

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"web-portfolio-api/internal"
+	"web-portfolio-api/internal/db"
+)
+
+func main() {
+	apiKey := os.Getenv("API_KEY")
+	port := "8080"
+	dbPath := "views.db"
+	
+	store, err := db.Open(dbPath)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+
+	viewsHandler := handler.viewsHandler
+}
